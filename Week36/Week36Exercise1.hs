b0308bffeb4b7ba0db065bf4660bca14bd798ef4
Hans <hansdevik@gmail.com>
2023-08-28 22:38:38 +0200

    week36 2b finnished

--- Week36/Week36Exercise1.hs
+++ Week36/Week36Exercise1.hs
@@ -1,4 +1,4 @@
 module Week36.Week36Exercise1 where
 
 f :: [Integer] -> [t] -> [(Integer, t)]
-f n t = zip n `reverse` t
+f n t = reverse $ zip n t
---
+++ Week36/Week36Exercise2.hs
@@ -0,0 +1,33 @@
+module Week36.Week36Exercise2 where
+
+-- a)
+
+firstHalf :: String -> String
+firstHalf s = take (length s `div` 2) s
+
+lastHalf :: String -> String
+lastHalf s = drop ((length s + 1) `div` 2) s
+
+semiRepetitive :: String -> Maybe String
+semiRepetitive s =
+    let headStr = firstHalf s
+        taleStr = lastHalf s
+
+    in if headStr == taleStr then
+        Just headStr
+        else
+            Nothing
+
+--b)
+
+getCenterChar :: String -> Char
+getCenterChar s = s !! ((length s - 1) `div` 2)
+
+decomposeSemiRepetitive :: String -> Maybe (String, Maybe Char)
+decomposeSemiRepetitive s = case semiRepetitive s of 
+    Nothing -> Nothing
+    Just str -> 
+        if even (length s) then
+            Just (str, Nothing)
+            else
+                Just(str, Just (getCenterChar s))
