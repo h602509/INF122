640713b0c075ba8a712fff4c30fd11be850f1633
Hans <hansdevik@gmail.com>
2023-09-18 10:43:34 +0200

    Oblig0 Oppgave1 ferdig

--- Oblig0/Oblig0.hs
+++ Oblig0/Oblig0.hs
@@ -1,6 +1,7 @@
 module Oblig0 where
 
 import qualified Data.Set as Set
+import Data.Char
 
 type Key = [(Char,Char)]
 type FrequencyTable = [(Char,Double)]
@@ -9,16 +10,36 @@ type Dictionary = Set.Set String
 
 
 encode :: Key -> String -> String
-encode k = undefined
+encode k = map (lookUp k)
+
+lookUp :: Key -> Char -> Char
+lookUp [] c = c
+lookUp [k] c | fst k == c = snd k
+             | otherwise = c
+lookUp (k:ks) c | fst k == c = snd k
+                | otherwise = lookUp ks c
 
 decode :: Key -> String -> String
-decode = undefined
+decode k = map (lookUpDecode k)
+
+lookUpDecode :: Key -> Char -> Char
+lookUpDecode [] c = c
+lookUpDecode [k] c | snd k == c = fst k
+                   | otherwise = c
+lookUpDecode (k:ks) c | snd k == c = fst k
+                      | otherwise = lookUpDecode ks c
 
 count :: String -> FrequencyTable
 count s = undefined
 
 caesar :: Alphabet -> Integer -> Key
-caesar alphabet shift = undefined
+caesar alphabet shift = map (\x -> (x, int2let $ (let2int x + fromInteger shift) `mod` length alphabet)) alphabet
+
+let2int :: Char -> Int
+let2int c = ord c - ord 'a'
+
+int2let :: Int -> Char 
+int2let n = chr (ord 'a' + n)
 
 loadFrequencyTable :: FilePath -> IO FrequencyTable
 loadFrequencyTable file = undefined
--- Test.hs
+++ Test.hs
@@ -1,14 +1,38 @@
-digits :: Integer -> [Integer]
-digits n = map read $ reverse $ digHelp $ show n
+import qualified Data.Set as Set
+import Data.Char (ord, chr)
 
-digHelp :: String -> [String]
-digHelp [] = []
-digHelp (x:xs) = [x] : digHelp xs 
+type Key = [(Char,Char)]
+type FrequencyTable = [(Char,Double)]
+type Alphabet = String
+type Dictionary = Set.Set String
 
-removeZero :: [[Integer]] -> [Maybe [Integer]]
-removeZero = map zHelp
 
-zHelp :: [Integer] -> Maybe [Integer]
-zHelp x = if sum x == 0 
-    then Nothing
-    else Just x
+caesar :: Alphabet -> Integer -> Key
+caesar alphabet shift = map (\x -> (x, int2let $ (let2int x + fromInteger shift) `mod` length alphabet)) alphabet
+
+
+let2int :: Char -> Int
+let2int c = ord c - ord 'a'
+
+int2let :: Int -> Char
+int2let n = chr (ord 'a' + n)
+
+encode :: Key -> String -> String
+encode k = map (lookUp k)
+
+lookUp :: Key -> Char -> Char
+lookUp [] c = c
+lookUp [k] c | fst k == c = snd k
+             | otherwise = c
+lookUp (k:ks) c | fst k == c = snd k
+                | otherwise = lookUp ks c
+
+decode :: Key -> String -> String
+decode k = map (lookUpDecode k)
+
+lookUpDecode :: Key -> Char -> Char
+lookUpDecode [] c = c
+lookUpDecode [k] c | snd k == c = fst k
+                   | otherwise = c
+lookUpDecode (k:ks) c | snd k == c = fst k
+                      | otherwise = lookUpDecode ks c
