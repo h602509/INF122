cd2538a765e7c6dbab73b9d5d777b7193f29325b
Hans <hansdevik@gmail.com>
2023-11-09 21:45:48 +0100

    Week43 ex ferdig innlevert OK 100%

--- Week43/Week43Exercise1.hs
+++ Week43/Week43Exercise1.hs
@@ -1,6 +1,5 @@
 
 module Week43Exercise1 where
-import Prelude hiding (fmap, Functor)
 
 
 -- Tre fra oppgave lagt inn fo testing
@@ -9,12 +8,8 @@ rt = Branch [-1,1,-1] [Branch [1,-1] [Branch [] []]]
 data RoseTree a = Branch a [RoseTree a]
   deriving (Eq, Show)
 
-class Functor f where
-  fmap :: (a -> b) -> f a -> f b
-
 instance Functor RoseTree where
   fmap f (Branch x subTrees) = Branch (f x) (map (fmap f) subTrees)
-
 
 productNodes :: (Num a) => RoseTree [a] -> RoseTree a
 -- productNodes = fmap (foldr (*) 1) roseT
--- Week43/Week43Exercise2.hs
+++ Week43/Week43Exercise2.hs
@@ -1,10 +1,9 @@
-{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+
 module Week43Exercise2 where
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-import Data.Map
-import Data.Set
 
 class IntegerGraph g where
   emptyGraph :: g
@@ -13,11 +12,33 @@ class IntegerGraph g where
   nodeInGraph :: Integer -> g -> Bool
   edgeInGraph :: Integer -> Integer -> g -> Bool
 
-type MyGraph node = Map node (Set node)
+type MyGraph = Map.Map Integer (Set.Set Integer)
 
-instance IntegerGraph (MyGraph Integer) where
+instance IntegerGraph MyGraph where
+  
   emptyGraph = Map.empty
   insertNode node = (Map.insert node) Set.empty 
-  insertEdge startNo= undefined
-  nodeInGraph = undefined
-  edgeInGraph = undefined
+
+
+  insertEdge :: Integer -> Integer -> MyGraph -> MyGraph
+  insertEdge startNo endNo graph = let
+    -- legger inn nodene i grafen hvis de ikke eksisterer
+    graph' = insertNode' startNo (Map.lookup startNo graph) graph
+    graph'' = insertNode' endNo (Map.lookup endNo graph') graph'
+    -- legger inn endNo i StartNo Value
+    in Map.adjust (Set.insert endNo) startNo graph''
+
+    where
+      insertNode' _ (Just _) graph = graph
+      insertNode' node Nothing graph = insertNode node graph
+
+  nodeInGraph = Map.member
+  edgeInGraph startNo endNo graph = case Map.lookup startNo graph of
+    Nothing -> False
+    Just dest -> Set.member endNo dest 
+
+
+graph :: (IntegerGraph g) => g
+graph = insertNode 3 . insertEdge 1 6 . insertEdge 1 8 . insertEdge 5 1 . insertEdge 5 8 . insertEdge 8 5 $ emptyGraph
+
+
